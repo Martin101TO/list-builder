ef9b55e47784e313a0f55e4f0c10d7f6c77d95a4
Kevin Li <87391349+Martin101TO@users.noreply.github.com>
2023-06-23 13:07:40 -0700

    created main page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Image } from 'react-native';
+
 import ImageViewer from '../components/ImageViewer';
+import Button from '../components/Button';
 
 const PlaceholderImage = require('/assets/front-image.jpg');
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <View style={styles.imageContainer}>
-        <ImageViewer placeholderImageSource={PlaceholderImage} />
-      </View>
-    <StatusBar style="auto" />
+        <View style={styles.imageContainer}>
+            <ImageViewer placeholderImageSource={PlaceholderImage} />
+        </View>
+        <View style={styles.footerContainer}>
+            <Button label="Army Lists" theme="listButton"/>
+            <Button label="Reference" theme="rulesButton"/>
+        </View>
+        <StatusBar style="auto" />
     </View>
   );
 }
@@ -29,6 +35,11 @@ const styles = StyleSheet.create({
     width: 320,
     height: 440,
     borderRadius: 18,
+  },
+  footerContainer:{
+    flex: 2/3,
+    alignItems: 'center',
+    padding: 10,
   }
 
 });
